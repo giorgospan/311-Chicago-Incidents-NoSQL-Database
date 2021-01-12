8fcbe9b43f681f1c30b64f7ed3fd7d199d6db023
George Panagiotopoulos <giorgospan96@gmail.com>
2021-01-12 20:30:22 +0200

    Added request import script

---
+++ import/import_request.py
@@ -0,0 +1,107 @@
+from pymongo import MongoClient
+import pandas as pd
+import os
+
+# Connect to our MongoDB
+URI = 'mongodb://localhost:27017'
+client = MongoClient(URI)
+db = client.chicago_db
+db.drop_collection('request')
+
+
+def insert(filename):
+    # Read csv into a df
+    df = pd.read_csv(INPUT_DIR + filename)
+
+    print("=" * len(filename))
+    print(filename)
+    print("=" * len(filename))
+    print()
+
+    new_column_names = {
+
+        # Common
+        'Creation Date': 'creation_date',
+        'Completion Date': 'completion_date',
+        'Status': 'status',
+        'Service Request Number': 'request_number',
+        'Street Address': 'street_address',
+        'Zip Code': 'zip_code',
+        'Zip Codes': 'zip_codes',
+        'Ward': 'ward',
+        'Wards': 'wards',
+        'Location': 'location',
+        'Historical Wards 2003-2015': 'historical_wards',
+        'Community Area': 'community_area',
+        'Community Areas': 'community_areas',
+        'Census Tracts': 'census_tracts',
+        'Police District': 'police_district',
+        'Type Of Service Request': 'request_type',
+        'Latitude': 'latitude',
+        'Longitude': 'longitude',
+        'X Coordinate': 'x_coordinate',
+        'Y Coordinate': 'y_coordinate',
+
+        # RequestType-specific
+        'Current Activity': 'current_activity',
+        'Most Recent Action': 'most_recent_action',
+        'Zip': 'zip_code',
+        'Ssa': 'ssa',
+        'License Plate': 'license_plate',
+        'Vehicle Color': 'vehicle_color',
+        'Vehicle Make/Model': 'vehicle_model',
+        'How Many Days Has The Vehicle Been Reported As Parked?': 'days_parked',
+        'Number Of Black Carts Delivered': 'num_of_black_carts',
+        'What Type Of Surface Is The Graffiti On?': 'graffiti_surface',
+        'Where Is The Graffiti Located?': 'graffiti_location',
+        'Number Of Potholes Filled On Block': 'potholes_filled',
+        'Number Of Premises Baited': 'premises_baited',
+        'Number Of Premises With Garbage': 'premises_with_garbage',
+        'Number Of Premises With Rats': 'premises_with_rats',
+        'What Is The Nature Of This Code Violation?': 'nature',
+        'If Yes, Where Is The Debris Located?': 'debris_location',
+        'Location Of Trees': 'tree_location'
+
+    }
+
+    # Remove duplicate rows
+    df = df[~df.duplicated()]
+
+    # Rename column names to match column names in our DB
+    df.columns = map(str.title, df.columns)
+    df.rename(columns=new_column_names, inplace=True)
+
+    # Drop location column [redundant]
+    df.drop(columns=['location'], inplace=True)
+
+    # Remove 'T' from dates
+    df['creation_date'] = df['creation_date'].str.replace('T', ' ')
+    df['completion_date'] = df['completion_date'].str.replace('T', ' ')
+
+    # Applies only on './data/311-service-requests-street-lights-one-out.csv'
+    df['request_type'] = df['request_type'].str.replace('Street Light Out', 'Street Light - 1/Out')
+
+    # Applies only on './data/311-service-requests-pot-holes-reported.csv'
+    df['request_type'] = df['request_type'].str.replace('Pot Hole in Street', 'Pothole in Street')
+
+    # Filter license plates
+    if 'license_plate' in df.columns:
+        df['license_plate'] = df[df['license_plate'].str.len() < 10]['license_plate']
+
+    df['location'] = [{'type': 'Point', 'coordinates': [long, lat]} for long, lat in zip(df.longitude, df.latitude)]
+    df.drop(columns=['longitude', 'latitude'], inplace=True)
+
+    # Exclude empty fields
+    out = [dict((k, v) for k, v in zip(df.columns, row) if v is not None and v == v) for row in df.values]
+
+    # Insert into our collection
+    db.request.insert_many(out)
+
+
+INPUT_DIR = './data/'
+input_files = [f for f in os.listdir(INPUT_DIR) if f.endswith('.csv')]
+input_files.remove('311-service-requests-vacant-and-abandoned-buildings-reported.csv')
+
+# Insert each csv file in our request collection
+for f in input_files:
+    insert(f)
